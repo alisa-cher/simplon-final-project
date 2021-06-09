157717ed02f4789a67f052c18e69d39f3c1a9555
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2021-06-09 08:44:54 +0200

    initial commit

---
+++ src/main/java/fr/formation/langexchange/LangexchangeApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.langexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangexchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/formation/langexchange/LangexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.langexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangexchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
