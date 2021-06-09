6a73052c938499059424a098f427ee2d114c7013
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2021-06-09 19:06:33 +0200

    Account service implementation, injected service dependency into account controller

--- src/main/java/fr/formation/langexchange/controllers/AccountController.java
+++ src/main/java/fr/formation/langexchange/controllers/AccountController.java
@@ -1,6 +1,7 @@
 package fr.formation.langexchange.controllers;
 
 import fr.formation.langexchange.domain.dtos.AccountCreate;
+import fr.formation.langexchange.services.AccountService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -8,9 +9,16 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
+
+    private final AccountService service;
+
+    public AccountController(AccountService service) {
+        this.service = service;
+    }
+
     @PostMapping("/signup")
     public void createAccount(@RequestBody @Valid AccountCreate dto){
-        System.out.println(dto);
+        service.create(dto);
     }
 }
 
--- src/main/java/fr/formation/langexchange/services/AccountServiceImpl.java
+++ src/main/java/fr/formation/langexchange/services/AccountServiceImpl.java
@@ -1,2 +1,25 @@
-package fr.formation.langexchange.services;public class AccountServiceImpl {
+package fr.formation.langexchange.services;
+
+import fr.formation.langexchange.domain.dtos.AccountCreate;
+import fr.formation.langexchange.domain.entities.Account;
+import fr.formation.langexchange.repositories.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accounts;
+
+    public AccountServiceImpl(AccountRepository accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public void create(AccountCreate dto) {
+        Account entity = new Account();
+        entity.setUsername(dto.getUsername());
+        entity.setIdentifier(dto.getIdentifier());
+        entity.setPassword(dto.getPassword());
+        accounts.save(entity);
+    }
 }
