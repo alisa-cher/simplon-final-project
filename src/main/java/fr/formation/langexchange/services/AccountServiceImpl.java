08eb5605addd022f72533b582b4c84c7f4dd1223
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2021-06-17 18:20:44 +0200

    User password encryption

--- src/main/java/fr/formation/langexchange/services/AccountServiceImpl.java
+++ src/main/java/fr/formation/langexchange/services/AccountServiceImpl.java
@@ -3,14 +3,20 @@ package fr.formation.langexchange.services;
 import fr.formation.langexchange.domain.dtos.AccountCreate;
 import fr.formation.langexchange.domain.entities.Account;
 import fr.formation.langexchange.repositories.AccountRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    private final PasswordEncoder encoder;
+
     private final AccountRepository accounts;
 
-    public AccountServiceImpl(AccountRepository accounts) {
+    public AccountServiceImpl(PasswordEncoder encoder, AccountRepository accounts) {
+        this.encoder = encoder;
         this.accounts = accounts;
     }
 
@@ -19,7 +25,9 @@ public class AccountServiceImpl implements AccountService {
         Account entity = new Account();
         entity.setUsername(dto.getUsername());
         entity.setIdentifier(dto.getIdentifier());
-        entity.setPassword(dto.getPassword());
+        String raw = dto.getPassword();
+        String encoded = encoder.encode(raw);
+        entity.setPassword(encoded);
         accounts.save(entity);
     }
 }
